41753b4fe840040f33d09147a55bfc6d4520640b
Egor Vasyakin <e.vasyakin@itevas.ru>
2022-10-14 17:17:12 +0300

    Create RecursiveCategory.vue

---
+++ src/parts/RecursiveCategory.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="category">
+        <p class="category-title">
+            <span class="level">{{ cat.level }} level</span>
+            <span class="id">[{{ cat.id }}]</span>
+            <router-link class="category-name" :to="{ name: 'category', params: { catUrl: cat.url }}">
+                {{ cat.name }}
+            </router-link>
+            <span class="virtual">{{ cat.virtual }}</span>
+        </p>
+        <div v-if="cat.subcats" class="category-subs">
+            <RecursiveCategory v-for="sub in cat.subcats" :key="sub.id" :cat="sub" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        cat: { type: Object },
+    },
+};
+</script>
